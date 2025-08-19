9ec2ad6f1364e7622cd4adee94b217736dd7458b
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2025-08-19 16:23:17 -0300

    first commit

---
+++ src/main/java/com/oiis/libs/java/spring/commons/BaseClientSsoSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.oiis.libs.java.spring.commons;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.Customizer;
+import java.util.List;
+
+@Configuration
+public class BaseClientSsoSecurityConfig {
+
+    @Autowired
+    private SsoAuthenticationCheckFilter ssoAuthenticationCheckFilter;
+
+    @Value("${app.security.public-endpoints}")
+    private List<String> publicEndpoints;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // ou coloque a origem específica do seu front
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(false); // ou true, se usar cookies/sessão
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()) // desabilita CSRF no novo padrão
+                .cors(Customizer.withDefaults())             // habilita CORS (pode customizar aqui)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(publicEndpoints.toArray(new String[0]))
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .addFilterBefore(ssoAuthenticationCheckFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+        ;
+        return http.build();
+    }
+}
